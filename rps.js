0abb3cfb4dd2d937f5fadba8b8e5c57f18a29e6f
programmingpatt <programmingpatt@protonmail.com>
2022-07-04 01:44:09 -0400

    Create a function that simulates a round of Rock Paper Scissors
    Created a function that takes a player's choice and the output of the computerPlay function as parameters. This function will determine who wins, if there was a tie, or if an unexpected value is received. The player's choice is currently pre-set and there is not yet a way to receive input.

--- rps.js
+++ rps.js
@@ -2,11 +2,43 @@ let rando = Math.floor(Math.random() * 3) + 1;
 
 function computerPlay(rando) {
     if (rando == 1) {
-        return 'Rock'}
+        return 'rock'
+    }
     else if (rando == 2) {
-        return 'Paper'}
+        return 'paper'
+    }
     else if (rando == 3) {
-        return 'Scissors'}
+        return 'scissors'
+    }
 }
 
-console.log(computerPlay(rando))
+function playRound(playerSelect, compSelect) {
+    if (playerSelect===compSelect) {
+        return 'tie'
+    }
+    else if (playerSelect==='rock' && compSelect === 'paper') {
+        return 'computer wins'
+    }
+    else if (playerSelect==='rock' && compSelect ==='scissors') {
+        return 'player wins'
+    }
+    else if (playerSelect==='paper' && compSelect ==='rock') {
+        return 'player wins'
+    }
+    else if (playerSelect==='paper' && compSelect ==='scissors') {
+        return 'computer wins'
+    }
+    else if (playerSelect==='scissors' && compSelect ==='rock') {
+        return 'computer wins'
+    }
+    else if (playerSelect==='scissors' && compSelect ==='paper') {
+        return 'player wins'
+    }
+    else {
+        return 'Cheating is Forbidden!'
+    }
+}
+
+const playerSelect = 'rock'
+const compSelect = computerPlay(rando)
+console.log(playRound(playerSelect, compSelect))
